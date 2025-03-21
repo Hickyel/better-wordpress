017ad0e1c9db25a1eb1746d9b5439d6c2f948d13
Hick_el <jocelroc43@gmail.com>
2025-03-21 14:12:49 -0400

    Structuration du plugin

---
+++ wp-content/plugins/better-wordpress/better-wordpress.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: Better WordPress
+ * Plugin URI: 
+ * Description: A plugin to enhance WordPress functionality
+ * Version: 1.0.0
+ * Author: Jocelyn Blais-Rochon
+ * Author URI: 
+ * Text Domain: better-wordpress
+ * Domain Path: /languages
+ * License: GPL v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * @package BetterWordPress
+ */
+
+// If this file is called directly, abort.
+if (!defined('WPINC')) {
+    die;
+}
